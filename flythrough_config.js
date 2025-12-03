15881ea7c6eb86dd17e5cc7c6e9dcb0abbe06122
Brea DellaFave <breardellafave@gmail.com>
2025-12-02 23:13:01 -0500

    urgues

--- flythrough_config.js
+++ flythrough_config.js
@@ -186,7 +186,7 @@ var config = {
             camera: {
                 latitude: 38.4479,
                 longitude: -78.8657,
-                zoom: 19
+                zoom: 17
             },
             
             location: {
